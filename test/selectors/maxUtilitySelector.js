eb6679eebd41b9668e58d6b80d700e2782864d3b
Darek Greenly <git@zielak.pl>
2017-07-31 20:43:59 +0200

    tests are working

--- test/selectors/maxUtilitySelector.js
+++ test/selectors/maxUtilitySelector.js
@@ -3,18 +3,18 @@ import MaxUtilitySelector from '../../src/selectors/maxUtilitySelector'
 import Utility from '../../src/core/utility'
 
 test.beforeEach(t => {
-  t.selector = new MaxUtilitySelector()
+  t.context.selector = new MaxUtilitySelector()
 })
 
 test('select with no utilities', t => {
   const utilities = new Map()
-  t.is(t.selector.select(utilities), null, `didn't return null`)
+  t.is(t.context.selector.select(utilities), null, `didn't return null`)
 })
 
 test('select with one utility', t => {
   const utilities = new Map()
   utilities.set('the only one', new Utility({value: 0.7}))
-  t.is(t.selector.select(utilities), 'the only one')
+  t.is(t.context.selector.select(utilities), 'the only one')
 })
 
 test('select with some utilities', t => {
@@ -23,5 +23,5 @@ test('select with some utilities', t => {
   utilities.set('best', new Utility({value: 1}))
   utilities.set('last', new Utility({value: 0.2}))
   utilities.set('second', new Utility({value: 0.7}))
-  t.is(t.selector.select(utilities), 'best')
+  t.is(t.context.selector.select(utilities), 'best')
 })
